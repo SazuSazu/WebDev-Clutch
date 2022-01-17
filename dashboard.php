e1c547f3a547eb9acb4d0d90b7162fe0b6c6ef4d
Sazu11 <75355470+Sazu11@users.noreply.github.com>
2022-01-17 18:54:38 +0800

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,52 @@
+<?php include('dbconnection.php'); ?>
+
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <title> Admin Dashboard | Group 11 </title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+<body>
+  <div class="sidebar">
+    <div class="logo-details">
+      <i class='bx bx-phone'></i>
+      <span class="logo_name">GROUP 11</span>
+    </div>
+      <ul class="nav-links">
+        <li>
+          <a href="admin.php" class="active">
+            <i class='bx bx-grid-alt' ></i>
+            <span class="links_name">Dashboard</span>
+          </a>
+        </li>
+        <li>
+          <a href="manageproduct.php">
+            <i class='bx bx-box' ></i>
+            <span class="links_name">Manage Product</span>
+          </a>
+        </li>
+        <li>
+          <a href="logout.php">
+            <i class='bx bx-log-out' ></i>
+            <span class="links_name">Logout</span>
+          </a>
+        </li>
+      </ul>
+  </div>
+  <section class="home-section">
+    <nav>
+      <div class="sidebar-button">
+        <i class='bx bx-menu sidebarBtn'></i>
+        <span class="dashboard">Dashboard</span>
+      </div>
+    </nav>
